9fae737419e46d598e0e1d7e61d88d6a23ccda9d
steven <12992@holbertonstudents.com>
2026-06-11 10:43:36 +0200

    cfix sur get_redis user 5

--- 2-get_redis.py
+++ 2-get_redis.py
@@ -10,6 +10,6 @@ prenom_1 = r.get("user:1")
 prenom_2 = r.get("user:2")
 prenom_3 = r.get("user:3")
 prenom_4 = r.get("user:4")
-prenom_5 = r.get("user:3")
+prenom_5 = r.get("user:5")
 
 print(prenom_1, prenom_2, prenom_3, prenom_4, prenom_5)
